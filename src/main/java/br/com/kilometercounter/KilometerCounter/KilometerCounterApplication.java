d9522c1c293ff612d5f87fa8acc5b8d889a5ee01
Rafael Andrade <rafaandrade2810@gmail.com>
2024-03-11 04:39:07 -0300

    Initial commit

---
+++ src/main/java/br/com/kilometercounter/KilometerCounter/KilometerCounterApplication.java
@@ -0,0 +1,13 @@
+package br.com.kilometercounter.KilometerCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilometerCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilometerCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kilometercounter/KilometerCounter/KilometerCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kilometercounter.KilometerCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilometerCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
