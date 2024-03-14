81240c468d01e842ffafc35a7f8ab90f7012248b
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-03-14 06:05:34 -0300

    Criação do RouteDataList

---
+++ src/main/java/br/com/kilometercounter/dtos/RouteDataList.java
@@ -0,0 +1,10 @@
+package br.com.kilometercounter.dtos;
+
+import br.com.kilometercounter.domain.Client;
+import br.com.kilometercounter.domain.Route;
+
+public record RouteDataList(Long id, Client destinationClient, Client originClient, double distance) {
+    public RouteDataList(Route route) {
+        this(route.getId(),route.getOriginClient(), route.getDestinationClient(), route.getDistance());
+    }
+}
