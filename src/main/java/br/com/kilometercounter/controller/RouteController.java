c4bde1ce0b8fb2a539335777db44509d200cedf7
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-03-14 06:06:01 -0300

    Implementação da requisição GET para o Route

--- src/main/java/br/com/kilometercounter/controller/RouteController.java
+++ src/main/java/br/com/kilometercounter/controller/RouteController.java
@@ -2,11 +2,15 @@ package br.com.kilometercounter.controller;
 
 import br.com.kilometercounter.domain.Route;
 import br.com.kilometercounter.dtos.RouteDataCreate;
+import br.com.kilometercounter.dtos.RouteDataList;
 import br.com.kilometercounter.repository.RouteRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/route")
@@ -20,6 +24,16 @@ public class RouteController {
     public void createRoute (@RequestBody @Valid RouteDataCreate data) {
         routeRepository.save(new Route(data));
     }
+
+    @GetMapping
+    public List<RouteDataList> findAllRoutes(){
+        List<Route> routes = routeRepository.findAllById(List.of());
+        return routes.stream()
+                .map(RouteDataList::new)
+                .collect(Collectors.toList());
+    }
+
+
 
     @DeleteMapping("/{id}")
     @Transactional
