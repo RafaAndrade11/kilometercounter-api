b76974f040683b3e5aaae30eecfcd4f746d976dd
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-04-11 21:07:13 -0300

    Adicionado configuração

---
+++ src/main/java/br/com/kilometercounter/config/WebConfig.java
@@ -0,0 +1,18 @@
+package br.com.kilometercounter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permitir todas as origens
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Cabeçalhos permitidos
+    }
+}
